2d5bc470315621bdbd687837bbf59b528eba8dca
Raul Alcantara <ralcanta@mit.edu>
2019-08-06 19:49:43 -0400

    to pause a JS function for a determined amount of time

---
+++ components/sleep.js
@@ -0,0 +1,5 @@
+export const sleep = (ms) => {
+    //it's supposed to pause the async function where it's called
+    return new Promise(resolve =>setTimeout(resolve, ms))
+}
+// module.exports =  sleep
