e8fbc5e7b875a6bf0da968f06d76e3d6bc1b160f
Raul Alcantara <ralcanta@mit.edu>
2019-08-06 19:47:02 -0400

    way of handling the images using json format

---
+++ assets/images.js
@@ -0,0 +1,132 @@
+const images = {
+    "energy":{
+        "1": {
+            icon: require('./img/energy_icons/energy_1.png'),
+            text: require('./img/energy_texts/text_energy_1.png')
+        },
+        "2": {
+            icon: require('./img/energy_icons/energy_2.png'),
+            text: require('./img/energy_texts/text_energy_2.png')
+        },
+        "3": {
+            icon: require('./img/energy_icons/energy_3.png'),
+            text: require('./img/energy_texts/text_energy_3.png')
+        },
+        "4": {
+            icon: require('./img/energy_icons/energy_4.png'),
+            text: require('./img/energy_texts/text_energy_4.png')
+        },
+        "5": {
+            icon: require('./img/energy_icons/energy_5.png'),
+            text: require('./img/energy_texts/text_energy_5.png')
+        },
+        "6": {
+            icon: require('./img/energy_icons/energy_6.png'),
+            text: require('./img/energy_texts/text_energy_6.png')
+        },
+        "7": {
+            icon: require('./img/energy_icons/energy_7.png'),
+            text: require('./img/energy_texts/text_energy_7.png')
+        },
+        "8": {
+            icon: require('./img/energy_icons/energy_8.png'),
+            text: require('./img/energy_texts/text_energy_8.png')
+        },
+        "9": {
+            icon: require('./img/energy_icons/energy_9.png'),
+            text: require('./img/energy_texts/text_energy_9.png')
+        },
+        "10": {
+            icon: require('./img/energy_icons/energy_10.png'),
+            text: require('./img/energy_texts/text_energy_10.png')
+        },
+   
+    },
+    "mood":{
+        "1": {
+            icon: require('./img/mood_icons/mood_1.png'),
+            text: require('./img/mood_texts/text_mood_1.png')
+        },
+        "2": {
+            icon: require('./img/mood_icons/mood_2.png'),
+            text: require('./img/mood_texts/text_mood_2.png')
+        },
+        "3": {
+            icon: require('./img/mood_icons/mood_3.png'),
+            text: require('./img/mood_texts/text_mood_3.png')
+        },
+        "4": {
+            icon: require('./img/mood_icons/mood_4.png'),
+            text: require('./img/mood_texts/text_mood_4.png')
+        },
+        "5": {
+            icon: require('./img/mood_icons/mood_5.png'),
+            text: require('./img/mood_texts/text_mood_5.png')
+        },
+        "6": {
+            icon: require('./img/mood_icons/mood_6.png'),
+            text: require('./img/mood_texts/text_mood_6.png')
+        },
+        "7": {
+            icon: require('./img/mood_icons/mood_7.png'),
+            text: require('./img/mood_texts/text_mood_7.png')
+        },
+        "8": {
+            icon: require('./img/mood_icons/mood_8.png'),
+            text: require('./img/mood_texts/text_mood_8.png')
+        },
+        "9": {
+            icon: require('./img/mood_icons/mood_9.png'),
+            text: require('./img/mood_texts/text_mood_9.png')
+        },
+        "10": {
+            icon: require('./img/mood_icons/mood_10.png'),
+            text: require('./img/mood_texts/text_mood_10.png')
+        },
+ 
+    },
+    "curiosity":{
+        "1": {
+            icon: require('./img/curiosity_icons/curiosity_1.png'),
+            text: require('./img/curiosity_texts/text_curiosity_1.png')
+        },
+        "2": {
+            icon: require('./img/curiosity_icons/curiosity_2.png'),
+            text: require('./img/curiosity_texts/text_curiosity_2.png')
+        }, 
+        "3": {
+            icon: require('./img/curiosity_icons/curiosity_3.png'),
+            text: require('./img/curiosity_texts/text_curiosity_3.png')
+        }, 
+        "4": {
+            icon: require('./img/curiosity_icons/curiosity_4.png'),
+            text: require('./img/curiosity_texts/text_curiosity_4.png')
+        }, 
+        "5": {
+            icon: require('./img/curiosity_icons/curiosity_5.png'),
+            text: require('./img/curiosity_texts/text_curiosity_5.png')
+        }, 
+        "6": {
+            icon: require('./img/curiosity_icons/curiosity_6.png'),
+            text: require('./img/curiosity_texts/text_curiosity_6.png')
+        }, 
+        "7": {
+            icon: require('./img/curiosity_icons/curiosity_7.png'),
+            text: require('./img/curiosity_texts/text_curiosity_7.png')
+        }, 
+        "8": {
+            icon: require('./img/curiosity_icons/curiosity_8.png'),
+            text: require('./img/curiosity_texts/text_curiosity_8.png')
+        }, 
+        "9": {
+            icon: require('./img/curiosity_icons/curiosity_9.png'),
+            text: require('./img/curiosity_texts/text_curiosity_9.png')
+        }, 
+        "10": {
+            icon: require('./img/curiosity_icons/curiosity_10.png'),
+            text: require('./img/curiosity_texts/text_curiosity_10.png')
+        }, 
+    }
+
+}
+export default images;
